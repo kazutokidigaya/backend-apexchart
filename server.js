50d47f23f5547216fb38e12d4584dae9d394b05f
karanT <2001karanpt@gmail.com>
2024-11-01 23:14:41 +0530

    incresing the headerstimeout

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import { connectDb } from "./config/db.js";
 import bookingRoutes from "./routes/bookingRoutes.js";
 import cors from "cors";
+import http from "http";
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
@@ -14,7 +15,13 @@ app.use(cors());
 app.use(express.json());
 app.use("/api", bookingRoutes);
 
-app.listen(PORT, () => {
+const server = http.createServer(app);
+
+// Set longer timeouts for Render’s requirements
+server.keepAliveTimeout = 120 * 1000; // 120 seconds
+server.headersTimeout = 120 * 1000; // 120 seconds
+
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
   connectDb();
 });
